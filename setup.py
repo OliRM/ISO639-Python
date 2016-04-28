4340ddbe8af232c7d22e5fee0c3bbd95fe5adf28
OliRM <oliver.may@my.jcu.edu.au>
2016-04-28 20:42:39 +0000

    Added unicode support and setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='ISO639',
+      version='1.0',
+      author='Oliver',
+      url='https://github.com/OliRM/ISO639-Python',
+      packages=['ISO639'],
+     )
